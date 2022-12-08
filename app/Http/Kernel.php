8853dea8466e018ea22c6b98869d47a045275f62
yano <yano@example.com>
2022-12-08 18:21:04 +0900

    laravel API with Cognito fin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'aws-cognito' => \Ellaisys\Cognito\Http\Middleware\AwsCognitoAuthenticate::class,
     ];
 }
--- config/cognito.php
+++ config/cognito.php
@@ -59,6 +59,7 @@ return [
     'cognito_user_fields'   => [
         'name' => 'name',
         'email' => 'email',
+        'password' => 'password',
     ],
 
     /*
@@ -97,7 +98,7 @@ return [
     | This option controls the SSO settings into the application.
     |
     */
-    'add_missing_local_user_sso'    => env('AWS_COGNITO_ADD_LOCAL_USER', false),
+    'add_missing_local_user_sso'    => env('AWS_COGNITO_ADD_LOCAL_USER', true),
     'delete_user'                   => env('AWS_COGNITO_DELETE_USER', false),
 
     // Package configurations
